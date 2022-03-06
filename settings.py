40b85d0c5f3e68fd2900397d5b04a3fc07d2b6ec
Manish Salunkhe <manishmaruti@iitbhilai.ac.in>
2022-03-06 23:24:26 +0530

    settings updated

--- settings.py
+++ settings.py
@@ -1,9 +1,26 @@
-# Screen setting
-WIDTH , HEIGHT = 500 , 700
+from pygame.math import Vector2 as vec
+
+# screen settings
+WIDTH, HEIGHT = 610, 670
 FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT-TOP_BOTTOM_BUFFER
 
-# Color setting
-# Player seting
-# Mob setting
+ROWS = 30
+COLS = 28
 
+# colour settings
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255, 255, 255)
+PLAYER_COLOUR = (190, 194, 15)
 
+# font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+# player settings
+# PLAYER_START_POS = vec(2, 2)
+
+# mob settings
